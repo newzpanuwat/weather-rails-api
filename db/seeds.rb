412828683926ccd9aefe31d8e18d92227fe6c579
newzpanuwat <new_sbh@me.com>
2017-06-01 21:55:36 +0700

    Initialize commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+l = Location.create(name: "New York")
+l.recordings.create(temp: 32, status: "Sunny")
+l.recordings.create(temp: 20, status: "Snow")
+l.recordings.create(temp: 26, status: "Cloudy")
+l.recordings.create(temp: 30, status: "Cloudy")
